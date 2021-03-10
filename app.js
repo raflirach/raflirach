a512a214905c4cdbe2eda8bfc4456c223402c529
raflirach <rafli060395@gmail.com>
2021-03-10 08:26:05 +0700

    add: app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const { connect } = require('./config/mongodb')
+const app = express()
+const PORT = process.env.PORT || 4001
+
+
+connect().then(() => {
+  const router = require('./routes')
+  // const errorHandler = require('./middlewares/errorHandler')
+
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: true }))
+  
+  app.use('/', router)
+  // app.use(errorHandler)
+  app.listen(PORT, () => {
+    console.log(`App listen to port : ${PORT}`);
+  })
+})
