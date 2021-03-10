d4673509e04bf16677be2106cb08ef8a908f9142
raflirach <rafli060395@gmail.com>
2021-03-10 19:04:00 +0700

    add: route index

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,13 @@ const router = require('express').Router()
 const AuthController = require('../controller/auth')
 const userRouter = require('./user')
 
+router.get('/', (req, res) => {
+  res.status(200).json({
+    repository: 'https://github.com/raflirach/raflirach',
+    documentation: 'https://github.com/raflirach/raflirach/blob/main/api_doc.md'
+  })
+})
+
 router.get('/token', AuthController.getToken)
 
 router.use('/users', userRouter)
